ae8e9e99ec63b92acfb4a5b6ed357f6d3018bd7f
Alex <example@gmail.com>
2022-07-09 15:54:32 +0300

    first practice

---
+++ first.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+const movieName = prompt("Один из последних просмотренных фильмов");
+const movieGrade = prompt("На сколько оцените его");
+
+personalMovieDB["movies"][movieName] = movieGrade;
+
+console.log(personalMovieDB["movies"][movieName]);
